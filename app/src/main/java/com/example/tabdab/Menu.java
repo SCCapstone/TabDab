d0c1531ef2ddd38c6681426119495199eaa63745
gufding <runbike9>
2020-12-14 22:14:54 -0500

    Created preliminary Menu class so vendors can make menus.

---
+++ app/src/main/java/com/example/tabdab/Menu.java
@@ -0,0 +1,13 @@
+package com.example.tabdab;
+
+import java.util.ArrayList;
+
+public class Menu {
+    ArrayList<BillItem> menu;
+
+    public Menu () {
+        this.menu = new ArrayList<BillItem>();
+    }
+
+
+}
