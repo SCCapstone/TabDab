05d043234aee95b44e70b220bef284b3b97c07e1
gufding <runbike9>
2021-01-26 22:19:21 -0500

    User can view previous payments.

--- app/src/main/java/com/example/tabdab/CreateBill.java
+++ app/src/main/java/com/example/tabdab/CreateBill.java
@@ -5,16 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,13 +21,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import androidmads.library.qrgenearator.QRGContents;
-import androidmads.library.qrgenearator.QRGEncoder;
-
-public class BillCreator extends AppCompatActivity {
+public class CreateBill extends AppCompatActivity {
   // Set up ui elements
   ScrollView scroller, itemizedBillScroller;
   LinearLayout itemized_bill_layout;
@@ -50,7 +43,7 @@ public class BillCreator extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_bill_creator);
+    setContentView(R.layout.activity_create_bill);
 
     // Set up the UI layout
     generateBtn = findViewById(R.id.generate_bill_btn);
