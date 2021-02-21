3866044e624e0b6eb4bd378ffab2acaf2b73d75d
RileyConant <rconant@email.sc.edu>
2021-02-21 12:43:08 -0500

    Moved validation checks for users from User Class to CreateAccount class. Modified errors to reflect the new checks.

--- app/src/main/java/com/example/tabdab/User.java
+++ app/src/main/java/com/example/tabdab/User.java
@@ -50,37 +50,17 @@ public class User {
   public void setFirstName (String name) {this.firstName = name;}
   public void setLastName (String name) {this.lastName = name;}
   public void setEmail (String email) {
-    if(validEmail(email)) {
       this.email = email;
-    }
-    else {
-      //unsure what to set it as?
-    }
   }
   public void setVendorID (String vendorID) {this.vendorID = vendorID;}
   public void setCardNum (String cardNum) {
-    if(validCardNum(cardNum)) {
       this.cardNum = cardNum;
-    }
-    else {
-      //unsure what to set it as?
-    }
   }
   public void setExpDate (String expDate) {
-    if(expDateChecker(expDate)) {
       this.expDate = expDate;
-    }
-    else {
-      //unsure what to set is as?
-    }
   }
   public void setCVV (String CVV) {
-    if(cvvChecker(CVV)) {
       this.CVV = CVV;
-    }
-    else {
-      //unsure what to set it as?
-    }
   }
   public void setPastPayments (List<Bill> pastPayments) {this.pastPayments = pastPayments;}
 
@@ -100,103 +80,5 @@ public class User {
 
   public String toString () {
     return this.firstName + " " + this.lastName + "\n" + this.email + "\n" + this.isVendor + "\n" + this.vendorID + "\n" + this.cardNum + "\n" + this.expDate + "\n" + this.CVV;
-  }
-
-  /**
-   * method that tests to see whether an email is a valid entry
-   * does not actually test if email actually exists
-   * @param email
-   * @return false and or a valid email if true
-   */
-  private static boolean validEmail(String email) {
-    String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-            "[a-zA-Z0-9_+&*-]+)*@" +
-            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-            "A-Z]{2,7}$";
-    Pattern pat = Pattern.compile(emailRegex);
-    if (email == null)
-      return false;
-    return pat.matcher(email).matches();
-  }
-
-  /**
-   *
-   * @param cardNum
-   * @return true or false depending if it ist a "valid" card num
-   * checks to see if the string is a whole integer
-   * then checks to see if the length requirement is right
-   * if met return true, else return false
-   */
-  private static boolean validCardNum(String cardNum) {
-    try {
-      // checking valid integer using parseInt() method
-      Long.parseLong(cardNum);
-      //typical american credit/ debit cards have a length between 12 and 15
-      if (cardNum.length() >= 12 || cardNum.length() <= 19) {
-        return true;
-      } //return false if length requirement isnt met
-      else {
-        return false;
-      }
-    }//return false if it isnt an integer to begin with
-    catch (NumberFormatException e) {
-      return false;
-    }
-  }
-
-  /**
-   * checks to see if the expDate is greater than or equal to the current mm/yyyy
-   * Also checks syntax and length
-   * @param expDate
-   * @return true or false
-   */
-  private static boolean expDateChecker(String expDate) {
-    //check to see length of input mm/yyyy
-    if (expDate.length() == 7) {
-      //check to see if its proper syntax
-      System.out.println("Length check");
-      SimpleDateFormat standard = new SimpleDateFormat("MM/yyyy");
-      standard.setLenient(false);
-      try {
-        Date userInput = standard.parse(expDate);
-        Date current = new Date();
-        boolean before = userInput.before(new Date());
-        //if the current date is before the user input string and or equal return true. Its valid
-        if (!before) {
-          return true;
-        } else if ((userInput.getMonth() == current.getMonth()) && (userInput.getYear() == current.getYear())) {
-          return true;
-        } else {
-          System.out.println("Sorry");
-          return false;
-        }
-      } catch (ParseException e) {
-        e.printStackTrace();
-      }
-    } else {
-      return false;
-    }
-    return false;
-  }
-
-  /**
-   * checks the length of it, cvv's can only be 3 or 4 in length
-   * @param CVV
-   * @return true or false based on if its a "valid" cvv number
-   */
-  private static boolean cvvChecker(String CVV) {
-    if (CVV.length() == 3 || CVV.length() == 4) {
-      try {
-        Integer.parseInt(CVV);
-        System.out.println("valid");
-        return true;
-      } catch (NumberFormatException e) {
-        System.out.println(CVV + " is not a valid cvv number");
-        return false;
-      }
-    } //cvv's can only be 3 and sometimes 4 in length
-    else {
-      return false;
-    }
   }
 }
