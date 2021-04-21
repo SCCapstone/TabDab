eaf5fa6c9c5a8d6537e0e536593b88fd16f6b554
MaximilianHensler <mhensler@email.sc.edu>
2021-04-21 13:37:53 -0400

    Small change to fix delete account bug. #183

--- app/src/main/java/com/example/tabdab/DeleteAccountFragment.java
+++ app/src/main/java/com/example/tabdab/DeleteAccountFragment.java
@@ -2,7 +2,9 @@ package com.example.tabdab;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,73 +23,86 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DeleteAccountFragment extends Fragment {
 
-    private Button ButDelAcc;
-    private TextView delAccPassword;
-    private   MainActivity ma;
-    private User user;
-    private FirebaseAuth fAuth;
-    private SharedPreferences sharedPreferences;
+  private Button ButDelAcc;
+  private TextView delAccPassword;
+  private MainActivity ma;
+  private User user;
+  private FirebaseAuth fAuth;
+  private SharedPreferences sharedPreferences;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ma = (MainActivity)getActivity();
-        user = ma.mainActGetUser();
-        fAuth = FirebaseAuth.getInstance();
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        // For logout purposes
-        sharedPreferences = ma.getSharedPreferences("Preferences",0);
-    }
-        @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_delete_account, container, false);
+    // Get the user information
+    ma = (MainActivity) getActivity();
+    user = ma.mainActGetUser();
+    fAuth = FirebaseAuth.getInstance();
 
-            ButDelAcc = view.findViewById(R.id.deleteAccountBut);
-            delAccPassword = view.findViewById(R.id.delAccPass);
+    // For logout purposes
+    sharedPreferences = ma.getSharedPreferences("Preferences", 0);
+  }
 
-           ButDelAcc.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String checkPass = delAccPassword.getText().toString().trim();
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_delete_account, container, false);
 
-                    AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), checkPass);
-                    fAuth.getCurrentUser().reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful()) {
-                                Toast.makeText(getContext(), "Account successfully deleted!", Toast.LENGTH_LONG).show();
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putString("LOGIN", null);
-                                editor.apply();
-                                fAuth.getCurrentUser().delete();
-                                startActivity(new Intent(ma.getApplicationContext(), Login.class));
-                            }
-                            else {
-                                delAccPassword.setError("Incorrect password");
-                            }
-                        }
-                    });
-                }
-            });
+    ButDelAcc = view.findViewById(R.id.butDelete);
+    delAccPassword = view.findViewById(R.id.delAccPass);
 
-            return view;
-        }
-
-    public DeleteAccountFragment() {
-        // Required empty public constructor
-    }
+    ButDelAcc.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String checkPass = delAccPassword.getText().toString().trim();
 
 
-    public static DeleteAccountFragment newInstance(User user) {
-       DeleteAccountFragment deleteAccountFragment = new DeleteAccountFragment();
-        Bundle args = new Bundle();
-        args.putString("user", user.toJson());
-        deleteAccountFragment.setArguments(args);
-        return deleteAccountFragment;
-    }
-    public static DeleteAccountFragment newInstance(){ return new DeleteAccountFragment(); }
+        AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), checkPass);
+        fAuth.getCurrentUser().reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+          @Override
+          public void onComplete(@NonNull Task<Void> task) {
+            if (task.isSuccessful()) {
+              // Update the shared preferences so the user is logged out properly
+              SharedPreferences.Editor editor = sharedPreferences.edit();
+              editor.putString("LOGIN", null);
+              editor.apply();
+
+              // Delete the user from firebase
+              fAuth.getCurrentUser().delete();
+              FirebaseDatabase.getInstance().getReference("users").child(user.getEmail().replace('.','*')).setValue(null);
+
+              Toast.makeText(getContext(), "Account successfully deleted!", Toast.LENGTH_LONG).show();
+
+              startActivity(new Intent(ma.getApplicationContext(), Login.class));
+            } else {
+              delAccPassword.setError("Incorrect password");
+            }
+          }
+        });
+      }
+    });
+
+    return view;
+  }
+
+  public DeleteAccountFragment() {
+    // Required empty public constructor
+  }
+
+
+  public static DeleteAccountFragment newInstance(User user) {
+    DeleteAccountFragment deleteAccountFragment = new DeleteAccountFragment();
+    Bundle args = new Bundle();
+    args.putString("user", user.toJson());
+    deleteAccountFragment.setArguments(args);
+    return deleteAccountFragment;
+  }
+
+  public static DeleteAccountFragment newInstance() {
+    return new DeleteAccountFragment();
+  }
 }
 
