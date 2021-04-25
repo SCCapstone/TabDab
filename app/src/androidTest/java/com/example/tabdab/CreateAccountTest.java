3c868b94c5ea3b2ec87ae81d4bd509574cbccc6f
RileyConant <rconant@email.sc.edu>
2021-04-25 14:59:25 -0400

    CreateAccountTest. #73

---
+++ app/src/androidTest/java/com/example/tabdab/CreateAccountTest.java
@@ -0,0 +1,191 @@
+package com.example.tabdab;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreateAccountTest {
+
+    @Rule
+    public ActivityTestRule<Login> mActivityTestRule = new ActivityTestRule<>(Login.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.CAMERA");
+
+    @Test
+    public void createAccountTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.LogEmail),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        //appCompatEditText.perform(replaceText("testtest1127@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.LogPass),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        //appCompatEditText2.perform(replaceText("Test11271999"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.loginButton), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.registerButton), withText("Register"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        //Now testing the create account screen
+
+        ViewInteraction appCompatRegEditText = onView(
+                allOf(withId(R.id.uFirstName),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatRegEditText.perform(replaceText("FirstName"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegEdit2Text = onView(
+                allOf(withId(R.id.uLastName),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatRegEdit2Text.perform(replaceText("LastName"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegEdit3Text = onView(
+                allOf(withId(R.id.uEmail),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatRegEdit3Text.perform(replaceText("testtest1127@gmail.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegEdit4Text = onView(
+                allOf(withId(R.id.uPassword),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatRegEdit4Text.perform(replaceText("Test11271999"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegEdit5Text = onView(
+                allOf(withId(R.id.uCardNum),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatRegEdit5Text.perform(replaceText("1234123412341234"), closeSoftKeyboard());
+
+
+        ViewInteraction appCompatRegEdit6Text = onView(
+                allOf(withId(R.id.uExpDate),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                6),
+                        isDisplayed()));
+        appCompatRegEdit6Text.perform(replaceText("12/3456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegEdit7Text = onView(
+                allOf(withId(R.id.uCVV),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                7),
+                        isDisplayed()));
+        appCompatRegEdit7Text.perform(replaceText("123"), closeSoftKeyboard());
+
+        ViewInteraction appCompatRegBut = onView(
+                allOf(withId(R.id.btnRegister), withText("Register"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                10),
+                        isDisplayed()));
+        appCompatRegBut.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+
+
+}
